2e0cbf217670bccaf38400b92fa95bbda8eba52c
Guillaume Desforges <aceus02@gmail.com>
2020-03-17 16:13:02 +0100

    Set nixpkgs pinned by default to head of stable 19.09, fix issue 4

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,13 +1,20 @@
-{ usePinnedNixpkgs ? false }:
+{ usePinnedNixpkgs
+, pinnedNixpkgsRef
+, pinnedNixpkgsUrl
+, pinnedNixpkgsRev
+}:
+
 # The version of nixpkgs used for this project
 let
   # Either a pinned version
-  get-pinned-nixpkgs = ref: rev:
-    builtins.fetchGit {
-        url = "https://github.com/NixOS/nixpkgs.git";
-        inherit ref rev;
-    };
-  pinned-nixpkgs = get-pinned-nixpkgs "master" "cca0c894a13ec398de8b046174239513197f5c74" {};
+  pinned-nixpkgs =
+    import (
+      builtins.fetchGit {
+        url = pinnedNixpkgsUrl;
+        ref = pinnedNixpkgsRef;
+        rev = pinnedNixpkgsRev;
+      }
+    ) {};
   
   # Or the current local
   current-local = import <nixpkgs> {};
